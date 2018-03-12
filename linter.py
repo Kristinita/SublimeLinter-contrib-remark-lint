7582ceb8899a080372f1f9a0b32b7f5905f60e08
James Sandford <james.sandford@bbc.co.uk>
2018-03-12 10:51:49 +0000

    Initial remark-lint commit

--- linter.py
+++ linter.py
@@ -1,10 +1,12 @@
-from SublimeLinter.lint import Linter  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
+from SublimeLinter.lint import Linter, util
 
 
-class __class__(Linter):
-    cmd = '__cmd__'
-    regex = r''
-    multiline = False
-    defaults = {
-        'selector': 'source.python'
-    }
+class RemarkLint(Linter):
+    syntax = ("markdown")
+    cmd = ("remark", "--use", "remark-preset-lint-markdown-style-guide")
+    regex = (
+        r'^\s+(?P<line>\d+):(?P<col>\d+)(?:-\d+:\d+)?\s{2}'
+        r'(?P<error>error)?(?P<warning>warning)?\s{2}'
+        r'(?P<message>(?:\S+)(?:\s{1}\S+)*)(?:\s{2,}\S+\s{2,}\S+)?$'
+        )
+    error_stream = util.STREAM_STDERR
